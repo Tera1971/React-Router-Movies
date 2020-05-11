d20509ba503ddc6197898aaf8f1b4b33100ea055
Tera Gilbert <52169004+Tera1971@users.noreply.github.com>
2020-05-11 13:40:45 -0400

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 
+import { Route } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 
 const App = () => {
-  const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
-
-  useEffect(() => {
-    const getMovies = () => {
-      axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
-          setMovieList(response.data);
-        })
-        .catch(error => {
-          console.error('Server Error', error);
-        });
-    }
-    getMovies();
-  }, []);
+  const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+    setSavedList( [...savedList, movie] );
   };
 
+  
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      <Route path= "/" exact component={MovieList} />;
+      <Route path = "/Movies/:id" 
+        component={Movie}
+        render= {routeProps => {
+          console.log("routeProps", routeProps);
+        }}  
+      />
+
     </div>
   );
 };
 
 export default App;
+
+
